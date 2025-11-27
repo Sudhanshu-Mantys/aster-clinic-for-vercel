3566581ef9f64dac5b3c35364ec1f76c2576f820
Sudhanshu Sahil <sudhanshu@mantys.io>
2025-11-27 10:42:35 +0530

    setup code

---
+++ renderer/tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    colors: {
+      // use colors only specified
+      white: colors.white,
+      gray: colors.gray,
+      blue: colors.blue,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
